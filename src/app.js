0eae52f2dacfd2b46501208796872b1ef74f9df5
mcglauflins <sarahriebschlager@gmail.com>
2022-02-10 02:04:49 +0000

    css issues left

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello Rigo from the console!");
+  let symbol = document.querySelector(".symbol");
+  let number = document.querySelector(".number");
+  let upsidesymbol = document.querySelector(".upsidesymbol");
+  let cardSymbol = ["♦", "♣", "♠", "♥"];
+  let cardNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let targetSymbol = Math.floor(Math.random() * cardSymbol.length);
+  let targetNumber = Math.floor(Math.random() * cardNumber.length);
+  number.innerHTML = `${cardNumber[targetNumber]}`;
+  symbol.innerHTML = `${cardSymbol[targetSymbol]}`;
+  upsidesymbol.innerHTML = `${cardSymbol[targetSymbol]}`;
 };
